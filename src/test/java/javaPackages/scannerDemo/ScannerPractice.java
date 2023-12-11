8d46cd59f9875cb4863b2e6db0b8adf5de96983d
Shifat Jahan <subojahan94@gmail.com>
2023-12-11 01:39:53 -0500

    updated code

--- src/test/java/javaPackages/scannerDemo/ScannerPractice.java
+++ src/test/java/javaPackages/scannerDemo/ScannerPractice.java
@@ -28,7 +28,7 @@ public class ScannerPractice {  // class name
         //  scan.nextFloat(): // taking input from user decimal  value
         int total = a+b;
         System.out.println("The total of a+b is = "+ total);
-        System.out.println("My Daughter name is Aaliyah");
+        System.out.println("My Daughter name is Aaliyahout ");
 
     }
 
